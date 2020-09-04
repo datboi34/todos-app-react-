0462ca05c5938a1c3e2b030a5c330b5afabd5095
datboi34 <edgar87250@gmail.com>
2020-09-03 21:11:14 -0400

    created a basic todo app html css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <nav class="nav">
+        <a href="#">
+          <img
+            class="avatar"
+            src="https://img.icons8.com/dusk/64/000000/cat-profile.png"
+          />
         </a>
-      </header>
-    </div>
+        <a href="#">Completed</a>
+        <a href="#">Options</a>
+        <a href="#">Contact</a>
+      </nav>
+      <div class="todo-list">
+        <h1>Todo List</h1>
+        <ul class="Weekly-duties">
+          <li>
+            <input type="checkbox" />
+            Buy eggs
+          </li>
+          <li>
+            {" "}
+            <input type="checkbox" />
+            Bacon eat
+          </li>
+          <li>
+            {" "}
+            <input type="checkbox" />
+            Go to the bank
+          </li>
+        </ul>
+        <a href="#">
+          <img
+            class="add-todo"
+            src="https://img.icons8.com/ios-filled/100/000000/plus.png"
+          />
+        </a>
+      </div>
+
+      <footer>
+        <h4> Icons obtained from:</h4>
+        <a href="https://icons8.com/icon/46393/cat-profile">
+          Cat Profile iconbyIcons8
+          <br></br>
+        </a>
+        <a href="https://icons8.com/icon/11255/plus">Plus icon by Icons8</a>
+      </footer>
+    </>
   );
 }
 
