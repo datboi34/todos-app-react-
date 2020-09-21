ff7bfe4a561581982d11d566cab0a068ee4d612a
datboi34 <edgar87250@gmail.com>
2020-09-21 13:11:51 -0400

    converted css into css in js

--- src/App.js
+++ src/App.js
@@ -25,48 +25,121 @@ const user = {
   username: "Freddy",
   image: "https://img.icons8.com/dusk/64/000000/cat-profile.png",
 };
+
 function App() {
   return (
     <>
-      <nav className="nav">
-        <a href="#">
-          <img className="avatar" src={user.image} />
-          <p className="user">{user.username}</p>
+      <nav style={navbar.nav}>
+        <a style={navbar.nav_a} href="#">
+          <img style={Styles.avatar} src={user.image} />
+          <p style={Styles.user}>{user.username}</p>
         </a>
-        <a href="#">Completed</a>
-        <a href="#">Options</a>
-        <a href="#">Contact</a>
+        <a style={navbar.nav_a} href="#">
+          Completed
+        </a>
+        <a style={navbar.nav_a} href="#">
+          Options
+        </a>
+        <a style={navbar.nav_a} href="#">
+          Contact
+        </a>
       </nav>
-      <div className="todo-list">
+      <div style={Styles.todo_list}>
         <h1>Todo List</h1>
-        <ul class="Weekly-duties">
+        <ul style={universal.ul}>
           {todoList.map((todo) => {
             return (
-              <li>
-                <p>{todo.description}</p>
-                <input type="checkbox" />
+              <li style={universal.ul}>
+                <p style={universal.ul}>{todo.description}</p>
+                <input
+                  style={universal.ul}
+                  type="checkbox"
+                  checked={todoList.completed}
+                />
               </li>
             );
           })}
         </ul>
         <a href="#">
           <img
-            className="add-todo"
+            style={img.plusImg}
             src="https://img.icons8.com/ios-filled/100/000000/plus.png"
           />
         </a>
       </div>
 
       <footer>
-        <h4> Icons obtained from:</h4>
-        <a href="https://icons8.com/icon/46393/cat-profile">
+        <h4 style={universal.footer}> Icons obtained from:</h4>
+        <a
+          style={universal.footer}
+          href="https://icons8.com/icon/46393/cat-profile"
+        >
           Cat Profile iconbyIcons8
           <br></br>
         </a>
-        <a href="https://icons8.com/icon/11255/plus">Plus icon by Icons8</a>
+        <a style={universal.footer} href="https://icons8.com/icon/11255/plus">
+          Plus icon by Icons8
+        </a>
       </footer>
     </>
   );
 }
+const universal = {
+  h1: {
+    margintop: "5px",
+  },
+  li: {
+    listStyle: "none",
+  },
+  ul: {
+    margin: "10px",
+  },
+  footer: {
+    marginLeft: "200px",
+  },
+};
 
+const navbar = {
+  nav: {
+    height: "100%",
+    width: "160px",
+    position: "fixed",
+    backgroundColor: "white",
+    top: "0",
+  },
+  nav_a: {
+    padding: "6px 8px 6px 16px",
+    textDecoration: "none",
+    fontSize: "25px",
+    color: "black",
+    display: "block",
+    borderBottom: "solid black 2px",
+    borderRight: "solid black 2px",
+  },
+  nav_a_hover: {
+    color: "blue",
+  },
+};
+
+const img = {
+  plusImg: {
+    marginTop: "50px",
+    marginLeft: "50px",
+  },
+};
+
+const Styles = {
+  user: {
+    textAlign: "center",
+    margin: "0",
+  },
+  avatar: {
+    marginTop: "20px",
+    marginLeft: "35px",
+    marginBottom: "7px",
+  },
+  todo_list: {
+    textAlign: "center",
+  },
+};
 export default App;
